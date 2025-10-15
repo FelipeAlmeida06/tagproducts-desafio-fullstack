74e688c86daa7ce926446ae832e2b71b5e07ba74
Felipe Antônio de O. Almeida <oliveira.lipe.2006@gmail.com>
2025-10-15 12:48:09 -0300

    Instalando as dependencias do front-end

---
+++ desafio-fullstack/front-end/form-produtos/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+// instalar lucide-react para os icones
+// npm install lucide-react
+
+// instalar tailwind css
+// npm install -D tailwindcss postcss autoprefixer
+// npx tailwindcss init -p
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
